e3b96a98a534c75622a701163a417a8418654269
HowdenG <graham_howden1@yahoo.co.uk>
2014-06-04 17:05:06 +0100

    register command + permission in plugin.yml and restrict command to the permission

--- src/main/java/com/publicuhc/tsverificationchecker/commands/VerificationCheckCommand.java
+++ src/main/java/com/publicuhc/tsverificationchecker/commands/VerificationCheckCommand.java
@@ -1,5 +1,6 @@
 package com.publicuhc.tsverificationchecker.commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
@@ -7,6 +8,8 @@ import org.bukkit.command.TabExecutor;
 import java.util.List;
 
 public class VerificationCheckCommand implements TabExecutor {
+
+    public static final String TS_VERIFY_PERMISSION = "tsverify.command";
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String s, String[] strings) {
@@ -16,7 +19,13 @@ public class VerificationCheckCommand implements TabExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
-        //TODO complete command
+        if(command.getName().equalsIgnoreCase("tsverify")) {
+            if(!sender.hasPermission(TS_VERIFY_PERMISSION)) {
+                sender.sendMessage(ChatColor.RED + "You need the permission (" + TS_VERIFY_PERMISSION + ") to run this command");
+                return true;
+            }
+            //TODO complete command
+        }
         return false;
     }
 }
