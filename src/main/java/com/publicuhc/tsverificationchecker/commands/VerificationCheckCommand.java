a5417cb63bf141f235c58adb2d5b64095125d9db
Graham <graham_howden1@yahoo.co.uk>
2014-06-04 20:45:32 +0100

    implement the command

--- src/main/java/com/publicuhc/tsverificationchecker/commands/VerificationCheckCommand.java
+++ src/main/java/com/publicuhc/tsverificationchecker/commands/VerificationCheckCommand.java
@@ -1,15 +1,30 @@
 package com.publicuhc.tsverificationchecker.commands;
 
+import com.publicuhc.tsverificationchecker.VerificationChecker;
+import org.apache.commons.lang.BooleanUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 
 public class VerificationCheckCommand implements TabExecutor {
 
     public static final String TS_VERIFY_PERMISSION = "tsverify.command";
+
+    private Plugin m_plugin;
+    private VerificationChecker m_checker;
+
+    public VerificationCheckCommand(Plugin plugin, VerificationChecker checker) {
+        m_plugin = plugin;
+        m_checker = checker;
+    }
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String s, String[] strings) {
@@ -18,13 +33,37 @@ public class VerificationCheckCommand implements TabExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(command.getName().equalsIgnoreCase("tsverify")) {
             if(!sender.hasPermission(TS_VERIFY_PERMISSION)) {
                 sender.sendMessage(ChatColor.RED + "You need the permission (" + TS_VERIFY_PERMISSION + ") to run this command");
                 return true;
             }
-            //TODO complete command
+            if(args.length <= 1) {
+                sender.sendMessage(ChatColor.RED + "Syntax: /tsverify true/false player1 player2 player3 OR /tsverify true/false *");
+                return true;
+            }
+            boolean onlineCheck = BooleanUtils.toBoolean(args[0]);
+
+            final HashMap<UUID, String> players = new HashMap<UUID, String>();
+
+            for(int i = 1; i < args.length - 1; i++) {
+                Player player = Bukkit.getPlayer(args[i]);
+                if(player == null) {
+                    continue;
+                }
+                players.put(player.getUniqueId(), player.getName());
+            }
+
+            Bukkit.getScheduler().runTaskAsynchronously(
+                    m_plugin,
+                    new AsyncVerificationCheck(
+                        sender,
+                        players,
+                        m_checker,
+                        onlineCheck
+                    )
+            );
         }
         return false;
     }
