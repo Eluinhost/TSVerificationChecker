8a726ae6d609225618f2f6c663f7d428ff58cb23
HowdenG <graham_howden1@yahoo.co.uk>
2014-06-10 12:32:01 +0100

    teamspeak account has a lastname

--- src/main/java/com/publicuhc/tsverificationchecker/models/TeamspeakAccount.java
+++ src/main/java/com/publicuhc/tsverificationchecker/models/TeamspeakAccount.java
@@ -3,6 +3,7 @@ package com.publicuhc.tsverificationchecker.models;
 public class TeamspeakAccount extends APIModel {
 
     private String m_uuid;
+    private String m_lastName;
 
     /**
      * @return the teamspeak UUID of the account
@@ -13,6 +14,15 @@ public class TeamspeakAccount extends APIModel {
 
     public TeamspeakAccount setUUID(String uuid) {
         m_uuid = uuid;
+        return this;
+    }
+
+    public String getLastName() {
+        return m_lastName;
+    }
+
+    public TeamspeakAccount setLastName(String lastName) {
+        m_lastName = lastName;
         return this;
     }
 }
