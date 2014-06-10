0af0f589b0639f48df63a537834b4effaea0a08b
HowdenG <graham_howden1@yahoo.co.uk>
2014-06-10 14:34:04 +0100

    use the httpclient library to fetch and use POST

--- src/main/java/com/publicuhc/tsverificationchecker/fetchers/URLFetcher.java
+++ src/main/java/com/publicuhc/tsverificationchecker/fetchers/URLFetcher.java
@@ -1,40 +1,43 @@
 package com.publicuhc.tsverificationchecker.fetchers;
 
 import com.publicuhc.tsverificationchecker.exceptions.FetchException;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
+import java.util.List;
 
 public class URLFetcher {
 
     /**
-     * Fetch the raw string from the URL
+     * Fetch the raw string from the URL using POST with the given parameters
      * @return String version of the contents
      * @throws FetchException
      */
-    public String fetch(String urlString) throws FetchException {
-        InputStream inputStream = null;
+    public String fetch(String urlString, List<NameValuePair> parameters) throws FetchException {
         try {
-            URL url = new URL(urlString);
-            inputStream = url.openStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-
-            StringBuilder jsonStringBuilder = new StringBuilder();
-            String tempLine;
-            while ((tempLine = reader.readLine()) != null) {
-                jsonStringBuilder.append(tempLine);
-            }
-            return jsonStringBuilder.toString();
-        } catch (IOException ioe) {
-            throw new FetchException();
-        } finally {
+            CloseableHttpClient client = HttpClients.createDefault();
+            HttpPost httpPost = new HttpPost(urlString);
+            httpPost.setEntity(new UrlEncodedFormEntity(parameters));
+            CloseableHttpResponse response = client.execute(httpPost);
             try {
-                if(inputStream != null)
-                    inputStream.close();
-            } catch (IOException ignored) {}
+                if (response.getStatusLine().getStatusCode() != 200) {
+                    throw new FetchException();
+                }
+                HttpEntity entity = response.getEntity();
+                String contents = EntityUtils.toString(entity);
+                EntityUtils.consumeQuietly(entity);
+                return contents;
+            } finally {
+                response.close();
+            }
+        } catch (Exception ex) {
+            throw new FetchException();
         }
     }
 }
