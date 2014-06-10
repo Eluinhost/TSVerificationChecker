1c65155a2ea83ab9447858b628c466d3dc58c400
HowdenG <graham_howden1@yahoo.co.uk>
2014-06-10 15:35:43 +0100

    go back to java.net stuff, much smaller plugin size then

--- src/main/java/com/publicuhc/tsverificationchecker/fetchers/URLFetcher.java
+++ src/main/java/com/publicuhc/tsverificationchecker/fetchers/URLFetcher.java
@@ -1,18 +1,35 @@
 package com.publicuhc.tsverificationchecker.fetchers;
 
 import com.publicuhc.tsverificationchecker.exceptions.FetchException;
-import org.apache.http.HttpEntity;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Iterator;
 import java.util.List;
 
 public class URLFetcher {
+
+    private String buildParameterString(List<NameValuePair> pairs) {
+        try {
+            StringBuilder params = new StringBuilder();
+            Iterator<NameValuePair> pairIterator = pairs.iterator();
+            while (pairIterator.hasNext()) {
+                NameValuePair pair = pairIterator.next();
+                params.append(pair.getKey())
+                        .append("=")
+                        .append(URLEncoder.encode(pair.getValue(), "utf-8"));
+                if(pairIterator.hasNext()) {
+                    params.append("&");
+                }
+            }
+            return params.toString();
+        }catch (UnsupportedEncodingException ex) {
+            ex.printStackTrace();
+            return "";
+        }
+    }
 
     /**
      * Fetch the raw string from the URL using POST with the given parameters
@@ -20,24 +37,43 @@ public class URLFetcher {
      * @throws FetchException
      */
     public String fetch(String urlString, List<NameValuePair> parameters) throws FetchException {
+        URL url;
+        HttpURLConnection connection = null;
         try {
-            CloseableHttpClient client = HttpClients.createDefault();
-            HttpPost httpPost = new HttpPost(urlString);
-            httpPost.setEntity(new UrlEncodedFormEntity(parameters));
-            CloseableHttpResponse response = client.execute(httpPost);
-            try {
-                if (response.getStatusLine().getStatusCode() != 200) {
-                    throw new FetchException();
-                }
-                HttpEntity entity = response.getEntity();
-                String contents = EntityUtils.toString(entity);
-                EntityUtils.consumeQuietly(entity);
-                return contents;
-            } finally {
-                response.close();
+            url = new URL(urlString);
+            connection = (HttpURLConnection)url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+            String urlParameters = buildParameterString(parameters);
+
+            connection.setRequestProperty("Content-Length", "" +
+                    Integer.toString(urlParameters.getBytes().length));
+            connection.setRequestProperty("Content-Language", "en-US");
+            connection.setUseCaches(false);
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+
+            DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+            wr.writeBytes (urlParameters);
+            wr.flush();
+            wr.close();
+
+            InputStream is = connection.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            String line;
+            StringBuilder response = new StringBuilder();
+            while((line = rd.readLine()) != null) {
+                response.append(line);
             }
+            rd.close();
+            return response.toString();
         } catch (Exception ex) {
             throw new FetchException();
+        } finally {
+            if(connection != null) {
+                connection.disconnect();
+            }
         }
     }
 }
